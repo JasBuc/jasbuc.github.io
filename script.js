57221eb6d99a51aca818ddfd4a1e6ff3932968a1
Jasmine Buckley <jasmine.buckley@softwire.com>
2021-02-26 12:12:14 +0000

    Added to script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const myHeading = document.querySelector('h2');
-let myImage = document.querySelector('img');
 
+//not working, need to debug
 
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'cauli.jpg') {
-        myHeading.textContent = 'This is a broccoli';
-        myImage.setAttribute('src','broc.png');
+let cauliImage = document.getElementById('cauli_img');
+cauliImage.OnClick = function() {
+    let cauliSrc = cauliImage.getAttribute('src');
+    if(cauliSrc === 'cauli.jpg') {
+        document.querySelector('h2').textContent = 'This is a broccoli';
+        cauliImage.setAttribute('src','broc.png');
         document.getElementById("link").style.visibility = "hidden";
     } else {
-        myHeading.textContent = 'This is a cauliflower';
-        myImage.setAttribute('src','cauli.jpg');
+        document.querySelector('h2').textContent = 'This is a cauliflower';
+        cauliImage.setAttribute('src','cauli.jpg');
         document.getElementById("link").style.visibility = "visible";
     }
 }
